715c2a3dd9fd977a6b620a1065c8e0a37bf36dc8
dariusz87 <dariusz.witkowicz@gmail.com>
2020-02-23 17:47:19 +0100

    added postresql env  variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +11,7 @@ from resources.store import Store, StoreList
 # from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'darek'
 api = Api(app)
@@ -25,7 +26,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Store, '/store/<string:name>')
 
 if __name__ == '__main__':
-    db.init_app(app)
+    # db.init_app(app)
     app.run(port=5000, debug=True)
 
 
